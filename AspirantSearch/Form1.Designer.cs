39f99bdc98e0eab3e7556fc1db08bf818c905751
Artur Vahanyan <artur.vaganyan96@gmail.com>
2015-10-21 05:31:02 -0800

    Init commit

---
+++ AspirantSearch/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace AspirantSearch
+{
+    partial class AspirantSearchingSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGetPerson = new System.Windows.Forms.Button();
+            this.lblSelectName = new System.Windows.Forms.Label();
+            this.txtSelectName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Person";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(83, 46);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(123, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(83, 85);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(123, 20);
+            this.txtSurname.TabIndex = 2;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(286, 46);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(110, 20);
+            this.txtAge.TabIndex = 3;
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(286, 85);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(110, 20);
+            this.txtAdress.TabIndex = 4;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(4, 52);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(4, 92);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(49, 13);
+            this.lblSurname.TabIndex = 6;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(235, 53);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 7;
+            this.lblAge.Text = "Age";
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(235, 92);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(39, 13);
+            this.lblAdress.TabIndex = 8;
+            this.lblAdress.Text = "Adress";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(154, 140);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(25, 13);
+            this.lblInfo.TabIndex = 9;
+            this.lblInfo.Text = "Info";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(200, 133);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(100, 20);
+            this.txtInfo.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(509, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(439, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnGetPerson
+            // 
+            this.btnGetPerson.Location = new System.Drawing.Point(727, 204);
+            this.btnGetPerson.Name = "btnGetPerson";
+            this.btnGetPerson.Size = new System.Drawing.Size(188, 26);
+            this.btnGetPerson.TabIndex = 12;
+            this.btnGetPerson.Text = "Get Person";
+            this.btnGetPerson.UseVisualStyleBackColor = true;
+            this.btnGetPerson.Click += new System.EventHandler(this.btnGetPerson_Click);
+            // 
+            // lblSelectName
+            // 
+            this.lblSelectName.AutoSize = true;
+            this.lblSelectName.Location = new System.Drawing.Point(506, 217);
+            this.lblSelectName.Name = "lblSelectName";
+            this.lblSelectName.Size = new System.Drawing.Size(68, 13);
+            this.lblSelectName.TabIndex = 13;
+            this.lblSelectName.Text = "Select Name";
+            // 
+            // txtSelectName
+            // 
+            this.txtSelectName.Location = new System.Drawing.Point(596, 210);
+            this.txtSelectName.Name = "txtSelectName";
+            this.txtSelectName.Size = new System.Drawing.Size(100, 20);
+            this.txtSelectName.TabIndex = 14;
+            // 
+            // AspirantSearchingSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 261);
+            this.Controls.Add(this.txtSelectName);
+            this.Controls.Add(this.lblSelectName);
+            this.Controls.Add(this.btnGetPerson);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Name = "AspirantSearchingSystem";
+            this.Text = "AspirantSearchingSystem";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGetPerson;
+        private System.Windows.Forms.Label lblSelectName;
+        private System.Windows.Forms.TextBox txtSelectName;
+    }
+}
+
---
+++ AspirantSearch/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace AspirantSearch
+{
+    public partial class AspirantSearchingSystem : Form
+    {
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        public AspirantSearchingSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection
+                (@"Data Source=Aspirant.sqlite;");
+        }
+
+        private void ExecuteQuery(string txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+
+        public DataTable GetDataTable(string sql)
+        {
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SQLiteConnection cnn = new SQLiteConnection("Data Source=Aspirant.sqlite;");
+
+                cnn.Open();
+
+                SQLiteCommand mycommand = new SQLiteCommand(cnn);
+
+                mycommand.CommandText = sql;
+
+                SQLiteDataReader reader = mycommand.ExecuteReader();
+
+                dt.Load(reader);
+
+                reader.Close();
+                cnn.Close();
+
+            }
+
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+
+            }
+            return dt;
+
+        }
+
+
+        private void Add()
+        {
+            try
+            {
+                string txtSQLQuery =
+                    "insert into  Aspirant (NAME,SURNAME,AGE,ADDRESS,INFO)" +
+                    " values ('" + txtName.Text + "','" + txtSurname.Text + "','" +
+                    txtAge.Text + "','" + txtAdress.Text + "','" + txtInfo.Text + "');";
+
+                txtName.Text = "";
+                txtSurname.Text = "";
+                txtAge.Text = "";
+                txtAdress.Text = "";
+                txtInfo.Text = "";
+
+                ExecuteQuery(txtSQLQuery);
+            }
+
+            catch (SQLiteException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void Get()
+        {
+
+            string query = "SELECT NAME,SURNAME,AGE,ADDRESS,INFO  FROM Aspirant Where NAME =  '"
+            + txtSelectName.Text + "';";
+
+            
+
+            DataTable dt;
+            dt = GetDataTable(query);
+
+            dataGridView1.DataSource = dt;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Add();
+
+        }
+
+        private void btnGetPerson_Click(object sender, EventArgs e)
+        {
+
+            Get();
+        }
+    }
+}
